b5cfbcfe77b17a529ec7d43600699dbcb94547f5
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-09-09 12:37:51 -0500

    Adding timer canvas

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 respawn = new Vector3(0,20,0);
+    
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.gameObject.transform.position.y < -20)
+            this.gameObject.transform.position = respawn;
     }
 }
